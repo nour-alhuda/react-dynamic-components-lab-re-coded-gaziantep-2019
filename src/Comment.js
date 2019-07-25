603c7cc7a89fae051cabeee1087171276997c617
nour-alhuda <nouralasali@hotmail.com>
2019-07-25 20:25:11 +0300

    done

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,15 @@
 // add Comment component here
+import React from 'react';
+
+
+  export default class Comment extends React.Component {
+  
+    render() {
+      return (
+        <div className="Comment">
+        {this.props.commentText}
+      </div> 
+      )
+    }
+    
+  }
